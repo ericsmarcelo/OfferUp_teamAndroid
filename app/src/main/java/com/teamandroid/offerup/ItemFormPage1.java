81b8fd7a13814f960fb23fba1a9bce981f67b775
oli001 <oli001@ucr.edu>
2018-02-21 13:13:04 -0800

    merging with master

--- app/src/main/java/com/teamandroid/offerup/ItemFormPage1.java
+++ app/src/main/java/com/teamandroid/offerup/ItemFormPage1.java
@@ -7,12 +7,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class ItemFormPage1 extends AppCompatActivity {
 
     private EditText itemName;
+    private String image;
+
+    private FirebaseAuth fbAuth;
+    private FirebaseAuth.AuthStateListener authListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +26,30 @@ public class ItemFormPage1 extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        itemName.setText("Enter Item Name");
+        itemName = (EditText) findViewById(R.id.itemName);
+
+        fbAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = fbAuth.getCurrentUser();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        fbAuth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(authListener != null) {
+            fbAuth.removeAuthStateListener(authListener);
+        }
     }
 
     public void toPageTwo(View view) {
+        String data = itemName.getText().toString();
         Intent intent = new Intent(this, ItemFormPage2.class);
+        intent.putExtra("ITEM_NAME", data);
         startActivity(intent);
     }
     public void toPageThree(View view) {
